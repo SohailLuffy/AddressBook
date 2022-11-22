80e9674b46a7e19b9088ffd92af9aadbba405e90
SohailLuffy <sohailislammail97@gmail.com>
2022-11-22 22:25:19 +0530

    Welcome to Address Book Program

---
+++ AddressBook/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book program.\n");
+            Console.ReadLine();
+        }
+    }
+}
